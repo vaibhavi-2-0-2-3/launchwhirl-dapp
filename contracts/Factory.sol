d863ce1ded7a89d7e098fb22f4a1689abcf2e87f
XboxSignOut <v.a.i.bhavi062024@gmail.com>
2025-06-18 12:14:23 +0530

    listed token for sale on blockchain

--- contracts/Factory.sol
+++ contracts/Factory.sol
@@ -1,12 +1,70 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.27;
 
+import {Token} from "./Token.sol";
+
 contract Factory {
     uint256 public immutable fee;
     address public owner;
 
+    uint256 public totalTokens;
+    address[] public tokens;
+    mapping(address => TokenSale) public tokenToSale;
+
+    struct TokenSale {
+        address token;
+        string name;
+        address creator;
+        uint256 sold;
+        uint256 raised;
+        bool isOpen;
+    }
+
+    // Events
+    event Created(address indexed token);
+    // event Buy(address indexed token, uint256 amount);
+
+
     constructor(uint256 _fee) {
         fee = _fee;
         owner = msg.sender;
+    }
+
+    function getTokenSale(
+        uint256 _index
+    ) public view returns (TokenSale memory) {
+        return tokenToSale[tokens[_index]];
+    }
+
+
+    function create(
+        string memory _name,
+        string memory _symbol
+    ) external payable {
+        //check if the fee is paid
+        require(msg.value >= fee, "Insufficient fee paid");
+
+        //create new token
+        Token token = new Token(msg.sender, _name, _symbol, 1_000_000 ether);
+
+        //save the token for later use
+        tokens.push(address(token));
+
+        totalTokens++;
+
+        //list the token for sale
+        TokenSale memory sale = TokenSale(
+            address(token),
+            _name,
+            msg.sender,
+            0,
+            0,
+            true
+        );
+
+        tokenToSale[address(token)] = sale;
+
+        //tell people it's live
+        emit Created(address(token));
     }
 }
--- test/Factory.js
+++ test/Factory.js
@@ -1,34 +1,93 @@
-const { loadFixture } = require("@nomicfoundation/hardhat-toolbox/network-helpers")
-const { expect } = require("chai")
-const { ethers } = require("hardhat")
+const {
+  loadFixture,
+} = require("@nomicfoundation/hardhat-toolbox/network-helpers");
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
 
 describe("Factory", function () {
   const FEE = ethers.parseUnits("0.01", 18);
 
-
   async function deployFactoryFixture() {
-    //fetch accounts
-    const [deployer] = await ethers.getSigners()
+    // Get accounts
+    const [deployer, creator, buyer] = await ethers.getSigners();
 
+    // Deploy factory
+    const Factory = await ethers.getContractFactory("Factory");
+    const factory = await Factory.deploy(FEE);
 
-    //fetch the contract
-    const Factory = await ethers.getContractFactory("Factory")
-    //deploy the contract
-    const factory = await Factory.deploy(FEE)
-    //return the contract instance
-    return { factory, deployer }
+    // Create token
+    const transaction = await factory
+      .connect(creator)
+      .create("DAPP Uni", "DAPP", { value: FEE });
+    await transaction.wait();
+
+    // Get token address
+    const tokenAddress = await factory.tokens(0);
+    const token = await ethers.getContractAt("Token", tokenAddress);
+
+    // Return values
+    return { factory, token, deployer, creator };
   }
 
   describe("Deployment", function () {
     it("Should set the fee", async function () {
-      const { factory } = await loadFixture(deployFactoryFixture)
-      expect(await factory.fee()).to.equal(FEE)
-    })
+      const { factory } = await loadFixture(deployFactoryFixture);
+      expect(await factory.fee()).to.equal(FEE);
+    });
 
     it("Should set the owner", async function () {
-      const { factory, deployer} = await loadFixture(deployFactoryFixture)
-      expect(await factory.owner()).to.equal(deployer.address)
-    })
-  })
-  
-})
+      const { factory, deployer } = await loadFixture(deployFactoryFixture);
+      expect(await factory.owner()).to.equal(deployer.address);
+    });
+  });
+
+  describe("Creating", function () {
+    it("Should set the owner", async function () {
+      const { factory, token } = await loadFixture(deployFactoryFixture);
+      expect(await token.owner()).to.equal(await factory.getAddress());
+    });
+
+    it("Should set the creator", async function () {
+      const { token, creator } = await loadFixture(deployFactoryFixture);
+      expect(await token.creator()).to.equal(creator.address);
+    });
+
+    it("Should set the supply", async function () {
+      const { factory, token } = await loadFixture(deployFactoryFixture);
+
+      const totalSupply = ethers.parseUnits("1000000", 18);
+
+      expect(await token.balanceOf(await factory.getAddress())).to.equal(
+        totalSupply
+      );
+    });
+
+    it("Should update ETH balance", async function () {
+      const { factory } = await loadFixture(deployFactoryFixture);
+
+      const balance = await ethers.provider.getBalance(
+        await factory.getAddress()
+      );
+
+      expect(balance).to.equal(FEE);
+    });
+
+    it("Should create the sale", async function () {
+      const { factory, token, creator } = await loadFixture(
+        deployFactoryFixture
+      );
+
+      const count = await factory.totalTokens();
+      expect(count).to.equal(1);
+
+      const sale = await factory.getTokenSale(0);
+      console.log(sale);
+
+      expect(sale.token).to.equal(await token.getAddress());
+      expect(sale.creator).to.equal(creator.address);
+      expect(sale.sold).to.equal(0);
+      expect(sale.raised).to.equal(0);
+      expect(sale.isOpen).to.equal(true);
+    });
+  });
+});
