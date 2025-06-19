d3d2916c3ccd18ea2e3f54dd98ac1d787d70ff1c
XboxSignOut <v.a.i.bhavi062024@gmail.com>
2025-06-19 12:45:20 +0530

    Added deposit and withdraw functions to Factory contract and corresponding tests

--- contracts/Factory.sol
+++ contracts/Factory.sol
@@ -6,7 +6,7 @@ import {Token} from "./Token.sol";
 contract Factory {
     uint256 public constant TARGET = 3 ether;
     uint256 public constant TOKEN_LIMIT = 500_000 ether;
-    
+
     uint256 public immutable fee;
     address public owner;
 
@@ -110,4 +110,31 @@ contract Factory {
 
         emit Buy(_token, _amount);
     }
+
+    function deposit(address _token) external {
+        // The remaining token balance and the ETH raised
+        // would go into a liquidity pool like Uniswap V3.
+        // For simplicity we'll just transfer remaining
+        // tokens and ETH raised to the creator.
+
+        Token token = Token(_token);
+        TokenSale memory sale = tokenToSale[_token];
+
+        require(sale.isOpen == false, "Factory: Target not reached");
+
+        // Transfer tokens
+        token.transfer(sale.creator, token.balanceOf(address(this)));
+
+        // Transfer ETH raised
+        (bool success, ) = payable(sale.creator).call{value: sale.raised}("");
+        require(success, "Factory: ETH transfer failed");
+    }
+
+    function withdraw(uint256 _amount) external {
+        require(msg.sender == owner, "Factory: Not owner");
+
+        (bool success, ) = payable(owner).call{value: _amount}("");
+        require(success, "Factory: ETH transfer failed");
+    }
+
 }
